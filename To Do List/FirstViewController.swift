3d7ebf952e03a6da0dc1e0df1083ab62c38f1cb0
Nadia <nadia@1s-MacBook-Air.local>
2017-08-03 15:57:32 +0300

    Initial Commit

---
+++ To Do List/FirstViewController.swift
@@ -0,0 +1,73 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Nadia on 8/2/17.
+//  Copyright © 2017 Serg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var items:[String] = []
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+            return items.count
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")  // pulling back items from permanent storage saved earlier
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        table.reloadData()
+    }
+    
+// removing items from table view with swipe
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+        }
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ To Do List/SecondViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Nadia on 8/2/17.
+//  Copyright © 2017 Serg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var addItemField: UITextField!
+    
+    @IBAction func AddPressed(_ sender: Any) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")  // pulling back items from permanent storage saved earlier
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(addItemField.text!)
+            
+        } else {
+            
+            items = [addItemField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")  // saving to permanent storage
+        
+        addItemField.text = "" //re-seting text field to be empty to add new items
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+// Shutting down keyboard when user taps out of the textfield
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+    }
+    
+// Shutting down the keyboard when user preses Return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+}
+
